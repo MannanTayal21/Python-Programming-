b1fde46c3d71a5c01c9c0077a5a2b039726b3055
Mannan Tayal <mannantayal21@gmail.com>
2025-09-15 09:44:49 +0530

    Add files via upload

---
+++ 60_DemonstrationofaFunction.py
@@ -0,0 +1,8 @@
+# Write a python code to demonstrate function.
+
+def greet(name):
+    print(f"Hello, {name}")
+    
+greet("Alice")
+
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)\n")
